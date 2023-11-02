1afc02f97ddb631ada9cceaad886bde797fc1790
JairSDS <jair@sdsharp.com>
2023-11-02 17:29:20 -0500

    feat: Task #2
    Sorting and filtering for grid native from agGrid

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -5,34 +5,38 @@ import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
 
 const columnDefs: ColDef[] = [
-  { field: "designation", headerName: "Designation" },
+  {
+    field: "designation",
+    headerName: "Designation",
+    filter: "agTextColumnFilter",
+  },
   {
     field: "discovery_date",
     headerName: "Discovery Date",
     filter: "agDateColumnFilter",
   },
-  { field: "h_mag", headerName: "H (mag)", filter: "agNumberColumnFilter" },
-  { field: "moid_au", headerName: "MOID (au)", filter: "agNumberColumnFilter" },
-  { field: "q_au_1", headerName: "q (au)", filter: "agNumberColumnFilter" },
-  { field: "q_au_2", headerName: "Q (au)", filter: "agNumberColumnFilter" },
+  { field: "h_mag", headerName: "H (mag)" },
+  { field: "moid_au", headerName: "MOID (au)" },
+  { field: "q_au_1", headerName: "q (au)" },
+  { field: "q_au_2", headerName: "Q (au)" },
   {
     field: "period_yr",
     headerName: "Period (yr)",
-    filter: "agNumberColumnFilter",
   },
   {
     field: "i_deg",
     headerName: "Inclination (deg)",
-    filter: "agNumberColumnFilter",
   },
   {
     field: "pha",
     headerName: "Potentially Hazardous",
+    filter: "agTextColumnFilter",
   },
   {
     field: "orbit_class",
     headerName: "Orbit Class",
     enableRowGroup: true,
+    filter: "agTextColumnFilter",
   },
 ];
 
@@ -40,9 +44,7 @@ const NeoGrid = (): JSX.Element => {
   return (
     <div className="ag-theme-alpine h-[calc(100vh-56px)] w-[100vw]">
       <div className="flex flex-row justify-center items-center w-full">
-        <h1 className="text-2xl font-semibold p-3">
-          Near-Earth Object Overview
-        </h1>
+        <h1 className="text-2xl font-semibold p-3">Near-Earth Object Overview</h1>
       </div>
       <AgGridReact
         rowData={data}
@@ -50,7 +52,7 @@ const NeoGrid = (): JSX.Element => {
         rowGroupPanelShow={"always"}
         defaultColDef={{
           sortable: true,
-          filter: "agTextColumnFilter",
+          filter: "agNumberColumnFilter",
         }}
       />
     </div>
