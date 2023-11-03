5eec3c1be789010660983b4d16d9bbf5bd79d84d
JairSDS <jair@sdsharp.com>
2023-11-02 21:36:45 -0500

    feat: Task #6
    Clear filters and sortings button

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -3,6 +3,8 @@ import { ColDef } from "ag-grid-community";
 import data from "./near-earth-asteroids.json";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
+import { useRef } from "react";
+import { SpacialObjct } from "./Models/tableData";
 
 var dateOptions: Intl.DateTimeFormatOptions = { year: "2-digit", month: "2-digit", day: "2-digit" };
 
@@ -50,12 +52,17 @@ const columnDefs: ColDef[] = [
 ];
 
 const NeoGrid = (): JSX.Element => {
+  const gridRef = useRef<AgGridReact<SpacialObjct>>(null);
   return (
     <div className=" ag-theme-alpine h-[calc(100vh-56px)] w-[100vw] max-w-[2000px]">
-      <div className="flex flex-row justify-center items-center w-full">
+      <div className="flex flex-row justify-start items-baseline">
         <h1 className="text-2xl font-semibold p-3">Near-Earth Object Overview</h1>
+        <button className="ml-[15px]" onClick={handleClear}>
+          Clear Filters and Sorters
+        </button>
       </div>
       <AgGridReact
+        ref={gridRef}
         rowData={data}
         columnDefs={columnDefs}
         rowGroupPanelShow={"always"}
@@ -68,6 +75,15 @@ const NeoGrid = (): JSX.Element => {
       />
     </div>
   );
+
+  function handleClear() {
+    if (gridRef.current) {
+      gridRef.current.api.setFilterModel(null);
+      gridRef.current.columnApi.applyColumnState({
+        defaultState: { sort: null },
+      });
+    }
+  }
 };
 
 function potHazardousCell(params: { value: string }) {
